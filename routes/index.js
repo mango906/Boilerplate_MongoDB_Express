ef31febea920695fe6f5c3276996aba6dfcc075b
mango906 <kirby@101.inc>
2021-01-19 23:53:26 +0900

    [feat]: HashTag 라우터를 추가한다

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import hashTagRouter from './hashTags';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('Hello, mobal-mobal Client. Server is running!');
 });
+
+router.use('/hashTags', hashTagRouter);
 export default router;
